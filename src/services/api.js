e60378e7da4071fb0a00ff3ab532ee52add5fb7e
leedongkyu <leedong0225@icloud.com>
2024-05-03 17:07:15 +0900

    feat: 답변하기post요청

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,15 @@
+const ROOT = `https://openmind-api.vercel.app/6-16/`;
 const postId = async (data = '') => {
-  if (data === '') {
+  if (!data) {
     return;
   }
-  const response = await fetch(
-    'https://openmind-api.vercel.app/6-16/subjects/',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json', // JSON 콘텐츠임을 명시
-      },
-      body: JSON.stringify({ name: data }),
+  const response = await fetch(`${ROOT}subjects/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json', // JSON 콘텐츠임을 명시
     },
-  );
+    body: JSON.stringify({ name: data }),
+  });
   if (!response.ok) {
     if (response.status === '') {
       return console.error(response.text());
@@ -20,4 +18,24 @@ const postId = async (data = '') => {
   }
   const result = await response.json();
   window.localStorage.setItem('id', result.id);
+};
+
+const postAnswer = async (questionId, data = '') => {
+  if (!data) {
+    return;
+  }
+  const response = await fetch(`${ROOT}questions/${questionId}/answers/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json', // JSON 콘텐츠임을 명시
+    },
+    body: JSON.stringify({ content: data, isRejected: false }),
+  });
+
+  if (!response.ok) {
+    if (response.status === '') {
+      return console.error(response.text());
+    }
+    return console.error(response.status);
+  }
 };

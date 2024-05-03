df05616e7e421e5718b176560da15fdba39e2486
baehyunji <wns038200@naver.com>
2024-05-04 01:14:04 +0900

    feat: 버튼 사이즈 관련 prop 및 css 추가

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,11 +1,12 @@
 import arrowRightIcon from '../../assets/images/ic_arrow_right.png';
 import styles from '../css/Button.module.css';
 
-const Button = ({ onClick, children, variant }) => {
+const Button = ({ onClick, children, variant, size }) => {
   const buttonClassName = `${styles.button} ${styles[variant] || ''}`;
+  const sizeClassName = `${styles[size] || ''}`;
 
   return (
-    <button className={buttonClassName} onClick={onClick}>
+    <button className={`${buttonClassName} ${sizeClassName}`} onClick={onClick}>
       {children}
       {variant === 'outline' && (
         <img src={arrowRightIcon} alt="화살표 아이콘" />

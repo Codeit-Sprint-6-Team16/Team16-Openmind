2b7a843f0e10b317d47a98c4df2690dd422e0439
kyj <dbwjd338@gmail.com>
2024-05-03 17:09:19 +0900

    feat : global.css 생성 및 페이지 마크업 구성

---
+++ src/components/ui/Logo.jsx
@@ -0,0 +1,7 @@
+import LogoIcon from '../../assets/images/logo.png';
+
+function Logo() {
+  return <img src={LogoIcon} alt="로고 아이콘" />;
+}
+
+export default Logo;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,26 @@
+import { Link } from 'react-router-dom';
+
+import styles from '../components/css/MainPage.module.css';
+import Input from '../components/ui/Input';
+import Logo from '../components/ui/Logo';
+
 function MainPage() {
-  return <></>;
+  return (
+    <main className="wrapper">
+      <div className={styles.mainWrap}>
+        {/* <Button /> */}
+        <div className={styles.center}>
+          <Link to="/">
+            <Logo />
+          </Link>
+          <form className={styles.form}>
+            <Input />
+            {/* <PullButon /> */}
+          </form>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default MainPage;

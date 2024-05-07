9a0c0feda0a16ecbbd721d7f288138cc3833cb68
leedongkyu <leedong0225@icloud.com>
2024-05-07 16:16:10 +0900

    fix useNavigation 삭제

--- src/components/containers/QuestionListContainer.jsx
+++ src/components/containers/QuestionListContainer.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate, useNavigation, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import { getQuestionList } from '@api/get.js';
 import { deleteProfile } from '@services/api/delete';

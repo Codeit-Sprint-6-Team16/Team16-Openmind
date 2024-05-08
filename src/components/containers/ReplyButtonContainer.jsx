58b3a17a984f87774ee3337307ded8a2a51825bd
leedongkyu <leedong0225@icloud.com>
2024-05-08 14:19:06 +0900

    feat ID값이 있어야 답변할수 있음

---
+++ src/components/containers/ReplyButtonContainer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Button from '@ui/Button';
+
+const ReplyButtonContainer = () => {
+  const navigate = useNavigate();
+  const onClickHandler = () => {
+    console.log(localStorage.getItem('ID'));
+    if (localStorage.getItem('ID')) {
+      return navigate(`/post/${localStorage.getItem('ID')}/answer`);
+    }
+    navigate('/');
+  };
+  return (
+    <Button variant={'outline'} onClick={onClickHandler}>
+      답변하러 가기
+    </Button>
+  );
+};
+
+export default ReplyButtonContainer;

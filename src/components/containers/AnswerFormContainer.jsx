70d040e2726bfd6f88e0d14529a57fb5acaebdb1
leedongkyu <leedong0225@icloud.com>
2024-05-08 17:01:22 +0900

    feat answer컨테이너 구체화

--- src/components/containers/AnswerFormContainer.jsx
+++ src/components/containers/AnswerFormContainer.jsx
@@ -2,7 +2,13 @@ import React, { useState } from 'react';
 
 import { postAnswer } from '@services/api/post';
 
-const AnswerFormContainer = ({ questionId, profile }) => {
+const AnswerFormContainer = ({}) => {
+  /*
+  "rejected"
+  "subjectId": 6023,
+  "answer": null
+  수정모드나 새로 작성하는 그를 경우로 대비해야 될 듯 
+  */
   const [content, setContent] = useState('');
   const submitAnswer = async () => {
     try {

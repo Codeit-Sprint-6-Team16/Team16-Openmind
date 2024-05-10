1ef4a87419f826b751efbfe3c948bdc549af140a
임희서 <imagic@imhuiseoui-MacBookAir.local>
2024-05-10 17:05:00 +0900

    무한 스크롤 시작

--- src/components/containers/ReactionContainer.jsx
+++ src/components/containers/ReactionContainer.jsx
@@ -31,7 +31,6 @@ function ReactionContainer({ question }) {
     }
   };
 
-  console.log(like, dislike);
   return (
     typeof like === 'number' &&
     typeof dislike === 'number' && (
--- src/components/containers/ToggleMenuContainer.jsx
+++ src/components/containers/ToggleMenuContainer.jsx
@@ -24,7 +24,7 @@ const ToggleMenuContainer = ({ onClick, order }) => {
   }, [isOpen]);
 
   return (
-    <ToggleMenu
+    <ToggleMenu //OrderMenu
       isOpen={isOpen}
       onClick={onClick}
       onToggleMenu={onToggleMenu}

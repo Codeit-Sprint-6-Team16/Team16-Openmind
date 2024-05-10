eddccdea4d367eb8dc0ab6f6e248a84ec4bf53f1
kyj <dbwjd338@gmail.com>
2024-05-10 16:26:46 +0900

    feat : prop으로 전달받은 데이터 ui에 적용

--- src/components/ui/ReactionPresenter.jsx
+++ src/components/ui/ReactionPresenter.jsx
@@ -1,5 +1,20 @@
-function ReactionPresenter() {
-  return <div></div>;
-}
+import likeGray from '@assets/images/image_like_gray.svg';
+import dislikeGray from '@assets/images/image_unlike_gray.svg';
+import styles from '@css/Reaction.module.css';
+
+const ReactionPresenter = ({ question }) => {
+  return (
+    <div className={styles.LikeDislike}>
+      <button className={styles.reactionButton}>
+        <img src={likeGray} alt="좋아요" />
+        <p>좋아요 {question.like}</p>
+      </button>
+      <button className={styles.reactionButton}>
+        <img src={dislikeGray} alt="싫어요" />
+        <p>싫어요 {question.dislike}</p>
+      </button>
+    </div>
+  );
+};
 
 export default ReactionPresenter;

1c561009763a9916fd4399dbec718f9ba2b4ae98
baehyunji <wns038200@naver.com>
2024-05-13 14:04:36 +0900

    fix: margin,padding 값 수정

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -11,7 +11,7 @@ function MainPage() {
   return (
     <main className="wrapper">
       <div className={styles.mainWrap}>
-        <div className="inner inner_main">
+        <div className="inner">
           <div className={styles.inner_order}>
             <div className={styles.right}>
               <Button
@@ -23,7 +23,7 @@ function MainPage() {
               </Button>
             </div>
             <Link to="/">
-              <Logo logoSize={'mobileSize'} />
+              <Logo logoSize={'mainLogo'} />
             </Link>
           </div>
           <div className={styles.center}>
